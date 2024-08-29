6f085dd5568bdd774410e5007c46cbd350c11d29
JokoCode <johannpereira1305@gmail.com>
2024-08-29 18:22:10 +0200

    V1.1

--- index.js
+++ index.js
@@ -10,45 +10,54 @@ const client = new Client({
     ]
 });
 
+
+
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
-
     let commandName = interaction.commandName;
 
     //Démarrage du PC
     if (commandName === 'démarrage') {
         await interaction.reply('Tentative de démarrage du PC...');
         exec('python3 control_pc.py démarrage', (error, stdout, stderr) => {
+
             if (error) {
                 interaction.followUp(`Erreur lors du démarrage : ${error.message}`);
                 return;
             }
+
             if (stderr) {
                 interaction.followUp(`Avertissement lors du démarrage : ${stderr}`);
                 return;
             }
             interaction.followUp(`Le PC est en train de démarrer : ${stdout}`);
         });
-    } 
+    }
+
     //Éteindre le PC
     else if (commandName === 'éteindre') {
         await interaction.reply('Tentative d\'arrêt du PC...');
         exec('python3 control_pc.py éteindre', (error, stdout, stderr) => {
+
             if (error) {
-                interaction.followUp(`Erreur lors de l'arrêt : ${error.message}`);
+                ineractzion.followUp(`Erreur lors de l'arrêt : ${error.message}`);
                 return;
             }
+
             if (stderr) {
                 interaction.followUp(`Avertissement lors de l'arrêt : ${stderr}`);
                 return;
             }
             interaction.followUp(`Le PC est en train de s'éteindre : ${stdout}`);
         });
-    } 
+    }
 });
+
+
 
 client.once('ready', async () => {
     console.log('Bot en ligne');
+
     const commands = [
         démarrage,
         éteindre
@@ -57,9 +66,11 @@ client.once('ready', async () => {
     const guild = await client.guilds.cache.first();
 
     guild.commands.set(commands)
+
         .then(() => {
             console.log('Commandes enregistrées avec succès.');
         })
+
         .catch(error => {
             console.error('Une erreur s\'est produite lors de l\'enregistrement des commandes :', error);
         });
